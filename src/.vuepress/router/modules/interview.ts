67ed225c89e8daf3e172da51da58be41ad263d74
shaohui_jin <1051131737@qq.com>
2024-04-15 19:58:15 +0800

    【调整】调整依赖、路由、iconfont的cdn

--- src/.vuepress/router/modules/interview.ts
+++ src/.vuepress/router/modules/interview.ts
@@ -3,38 +3,75 @@ import { SidebarObjectOptions } from '../interface';
 export const interviewSidebar: SidebarObjectOptions = {
   '': [
     {
-      text: 'JavaScript',
-      // collapsible: true,
-      icon: 'iconfont icon-javascript',
-      activeMatch: '/JavaScript/(.*)$',
-      children: [
-        { text: '1 - 闭包以及内存泄漏原因', link: '/JavaScript/Closure/' },
-        { text: '2 - 浏览器进程模型', link: '/JavaScript/BrowserProcessModel/' },
-        { text: '3 - 浏览器渲染原理', link: '/JavaScript/BrowserRenderingPrinciples/' },
-        { text: '4 - ECMAScript 6', link: '/JavaScript/ES6/' },
-      ]
-    },
-    {
       text: 'CSS',
-      // collapsible: true,
       icon: 'iconfont icon-css',
       activeMatch: '/CSS/(.*)$',
       children: [
-        { text: '1 - CSS 盒模型', link: '/CSS/BoxModel/' },
-        { text: '2 - CSS 选择器特性', link: '/CSS/SelectorFeatures/' },
-        { text: '3 - CSS 隐藏元素', link: '/CSS/HiddenElements/' },
-        { text: '4 - CSS 居中定位', link: '/CSS/Center/' },
-        { text: '5 - CSS 字体大小', link: '/CSS/FontSize/' },
+        { text: '├─ CSS 面试题总纲', link: '/CSS/InterviewQuestionOutline/', },
+        {
+          text: '├─ 理论',
+          collapsible: true,
+          children: [
+            { text: '├─ 盒模型', link: '/CSS/BoxModel/' },
+            { text: '└─ 选择器特性', link: '/CSS/SelectorFeatures/' },
+          ]
+        },
+        {
+          text: '└─ 样式',
+          collapsible: true,
+          children: [
+            { text: '├─ 字体大小', link: '/CSS/FontSize/' },
+            { text: '├─ 居中定位', link: '/CSS/Center/' },
+            { text: '└─ 隐藏元素', link: '/CSS/HiddenElements/' },
+          ]
+        },
+      ]
+    },
+    {
+      text: 'JavaScript',
+      icon: 'iconfont icon-javascript',
+      activeMatch: '/JavaScript/(.*)$',
+      children: [
+        { text: '├─ JavaScript 面试题总纲', link: '/JavaScript/InterviewQuestionOutline/', },
+        {
+          text: '├─ 函数',
+          collapsible: true,
+          children: [
+            { text: '└─ 闭包以及内存泄漏原因', link: '/JavaScript/Closure/' }
+          ]
+        },
+        {
+          text: '├─ 浏览器',
+          collapsible: true,
+          children: [
+            { text: '├─ 浏览器进程模型', link: '/JavaScript/BrowserProcessModel/' },
+            { text: '└─ 浏览器渲染原理', link: '/JavaScript/BrowserRenderingPrinciples/' }
+          ]
+        },
+        {
+          text: '└─ ECMAScript',
+          collapsible: true,
+          children: [
+            { text: '└─ ECMAScript 6', link: '/JavaScript/ES6/' }
+          ]
+        },
+      ]
+    },
+    {
+      text: 'TypeScript',
+      icon: 'iconfont icon-typescript',
+      activeMatch: '/TypeScript/(.*)$',
+      children: [
+        { text: '└─ 说说你对 TypeScript 中类的理解', link: '/TypeScript/Class/'},
       ]
     },
     {
       text: 'Vue',
-      // collapsible: true,
       icon: 'iconfont icon-vuejs',
       activeMatch: '/Vue/(.*)$',
       children: [
-        { text: '1 - Vue 生命周期', link: '/Vue/LifeCycle/' },
-        { text: '2 - Vue 通讯方式', link: '/Vue/Communication/' },
+        { text: '├─ Vue 生命周期', link: '/Vue/LifeCycle/' },
+        { text: '└─ Vue 通讯方式', link: '/Vue/Communication/' },
       ],
     },
     {
@@ -43,7 +80,7 @@ export const interviewSidebar: SidebarObjectOptions = {
       activeMatch: '/React/(.*)$',
       icon: 'iconfont icon-react',
       children: [
-        { text: '1 - React 通讯方式', link: '/React/Communication/' },
+        { text: '└─ React 通讯方式', link: '/React/Communication/' },
       ],
     },
     {
@@ -52,7 +89,7 @@ export const interviewSidebar: SidebarObjectOptions = {
       activeMatch: '/Micro/(.*)$',
       icon: 'iconfont icon-micro',
       children: [
-        { text: '1 - 乾坤', link: '/Micro/QianKun/' },
+        { text: '└─ 乾坤', link: '/Micro/QianKun/' },
       ],
     }
   ],
