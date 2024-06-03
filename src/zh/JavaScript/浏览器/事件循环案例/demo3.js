ee7b223efe2cbf7be4e8e4e2710a33ccc3595474
shaohui_jin <1051131737@qq.com>
2024-06-03 17:52:44 +0800

    【调整】事件循环

---
+++ src/zh/JavaScript/浏览器/事件循环案例/demo3.js
@@ -0,0 +1,29 @@
+setTimeout(()  => {
+  console.log('set1');
+  new Promise((resolve)  => {
+    resolve();
+  }).then(() => {
+    new Promise((resolve) => {
+      resolve();
+    }).then(() =>{
+      console.log('then4');
+    });
+    console.log('then2');
+  });
+});
+new Promise((resolve) => {
+  console.log('pr1');
+  resolve();
+}).then(() => {
+  console.log('then1');
+});
+setTimeout(()  => {
+  console.log("set2");
+});
+console.log(2);
+new Promise((resolve)  => {
+  resolve();
+}).then(() => {
+  console.log('then3');
+})
+// pr1  2  then1  then3  set1  then2  then4  set2
