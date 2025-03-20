925a1daec38a5cb187be7919dfbbf88fed4a4e8c
shaohui_jin <1051131737@qq.com>
2025-03-20 23:20:50 +0800

    路由文章调整

--- src/.vuepress/router/modules/frontEnd.ts
+++ src/.vuepress/router/modules/frontEnd.ts
@@ -3,127 +3,106 @@ import { SidebarObjectOptions } from '../interface';
 export const frontEndSidebar: SidebarObjectOptions = {
   '/FrontEnd/': [
     {
-      text: '2 - CSS',
-      icon: 'iconfont icon-space_demo',
+      text: 'CSS收藏模版',
+      collapsible: true,
+      children: [
+        'CssComponent/Checked/',
+        'CssComponent/Login/',
+        'CssComponent/Shutters/',
+        'CssComponent/StreamerButton/',
+      ]
+    },
+    {
+      text: '图形技术',
+      collapsible: true,
+      children: [
+        'Draw/Svg/',
+        'Draw/Canvas/'
+      ]
+    },
+    {
+      text: 'JavaScript',
       collapsible: true,
       children: [
         {
-          text: '2.1 - 收藏模版',
-          collapsible: false,
+          text: '理论知识',
+          collapsible: true,
           children: [
-            { text: '2.1.1 - 打钩动画', link: 'CssComponent/Checked/' },
-            { text: '2.1.2 - 登录页', link: 'CssComponent/Login/' },
-            { text: '2.1.3 - 百叶窗', link: 'CssComponent/Shutters/' },
-            { text: '2.1.4 - 流光按钮', link: 'CssComponent/StreamerButton' },
+            { text: 'Reflect', link: 'JavaScript/Reflect/' },
+            { text: '面试问题', link: 'JavaScript/Question/' },
+          ]
+        },
+        {
+          text: '代码题',
+          collapsible: true,
+          children: [
+            { text: '请使用JS完成一个LRU缓存', link: 'JavaScript/LRU/' },
+            { text: '请封装一个Storage', link: 'JavaScript/Storage/' },
+            { text: '请使用JS完成一个JSON扁平化', link: 'JavaScript/FlatJson/' },
           ]
         },
       ]
     },
     {
-      text: '3 - 图形技术',
-      icon: 'iconfont icon-bim_donghua',
+      text: '打包配置',
       collapsible: true,
       children: [
-        { text: '3.1 - Svg 入门', link: 'Draw/Svg/' },
-        { text: '3.1 - Canvas 入门', link: 'Draw/Canvas/' },
+        { text: '(dev)Dependencies', link: 'Configuration/Dependencies/' },
       ]
     },
     {
-      text: '4 - JavaScript',
-      icon: 'iconfont icon-javascript',
+      text: 'VUE',
       collapsible: true,
       children: [
         {
-          text: '4.1 - 理论知识',
+          text: '知识模块',
           collapsible: true,
           children: [
-            { text: '4.1.2 - Reflect', link: 'JavaScript/Reflect/' },
-            { text: '4.1.3 - 面试问题', link: 'JavaScript/Question/' },
+            { text: '面试问题', link: 'Vue/Question/' },
           ]
         },
         {
-          text: '4.3 - 小技巧',
+          text: '源码解读',
           collapsible: true,
           children: [
-            { text: '4.3.1 - JavaScript 小技巧', link: 'JavaScript/Skill/' },
+            { text: 'VUE3工具函数源码解析', link: 'Vue3/ToolFunction/' },
           ]
         },
         {
-          text: '4.4 - 代码题',
+          text: 'VUE 指令',
           collapsible: true,
           children: [
-            { text: '4.4.1 - 请使用JS完成一个LRU缓存', link: 'JavaScript/LRU/' },
-            { text: '4.4.2 - 请封装一个Storage', link: 'JavaScript/Storage/' },
-            { text: '4.4.3 - 请使用JS完成一个JSON扁平化', link: 'JavaScript/FlatJson/' },
-          ]
-        },
-      ]
-    },
-    {
-      text: '5 - 打包配置',
-      icon: 'iconfont icon-dabaopeizhi',
-      collapsible: true,
-      children: [
-        { text: '5.1 - 手写Webpack', link: 'Configuration/Webpack/' },
-        { text: '5.2 - (dev)Dependencies', link: 'Configuration/Dependencies/' },
-      ]
-    },
-    {
-      text: '6 - VUE',
-      icon: 'iconfont icon-vue',
-      collapsible: true,
-      children: [
-        {
-          text: '6.1 - 知识模块',
-          collapsible: true,
-          children: [
-            { text: '6.1.1 - 面试问题', link: 'Vue/Question/' },
+            { text: '点击组件外部', link: 'Vue/Directive/ClickOutside/' },
           ]
         },
         {
-          text: '6.2 - 源码解读',
+          text: '小技巧',
           collapsible: true,
           children: [
-            { text: '6.2.1 - VUE3工具函数源码解析', link: 'Vue3/ToolFunction/' },
-          ]
-        },
-        {
-          text: '6.3 - VUE 指令',
-          collapsible: true,
-          children: [
-            { text: '6.3.1 - 点击组件外部', link: 'Vue/Directive/ClickOutside/' },
-          ]
-        },
-        {
-          text: '6.4 - 小技巧',
-          collapsible: true,
-          children: [
-            { text: '6.4.1 - 懒加载', link: 'Vue/Skill/LazyLoader/' },
-            { text: '6.4.2 - 数据冻结', link: 'Vue/Skill/Freeze/' },
-            { text: '6.4.3 - 上下文批量引入', link: 'Vue/Skill/Context/' },
-            { text: '6.4.4 - 父组件里监听子组件的生命周期', link: 'Vue/Skill/@Hook/' },
-            { text: '6.4.5 - Computed中使用this', link: 'Vue/Skill/Computed/' },
-            { text: '6.4.6 - 父组件通信', link: 'Vue/Skill/Sync/' },
-            { text: '6.4.7 - 插槽', link: 'Vue/Skill/Slot/' },
+            { text: '懒加载', link: 'Vue/Skill/LazyLoader/' },
+            { text: '数据冻结', link: 'Vue/Skill/Freeze/' },
+            { text: '上下文批量引入', link: 'Vue/Skill/Context/' },
+            { text: '父组件里监听子组件的生命周期', link: 'Vue/Skill/@Hook/' },
+            { text: 'Computed中使用this', link: 'Vue/Skill/Computed/' },
+            { text: '父组件通信', link: 'Vue/Skill/Sync/' },
+            { text: '插槽', link: 'Vue/Skill/Slot/' },
           ]
         },
       ]
     },
     {
-      text: '7 - React篇',
-      icon: 'iconfont icon-react',
+      text: 'React篇',
       collapsible: true,
       children: [
-        { text: '7.1 - 浅谈12个Hooks', link: 'React/Hooks/' },
+        { text: '浅谈12个Hooks', link: 'React/Hooks/' },
       ]
     },
     {
-      text: '9 - 性能优化',
-      icon: 'iconfont icon-creative',
+      text: '性能优化',
       collapsible: true,
       children: [
-        { text: '9.1 - 浅谈性能优化', link: 'Performance/info/' },
-        { text: '9.2 - 性能优化方法', link: 'Performance/Skill/' },
+        { text: '浅谈性能优化', link: 'Performance/info/' },
+        { text: '性能优化方法', link: 'Performance/Skill/' },
       ]
     },
   ],
